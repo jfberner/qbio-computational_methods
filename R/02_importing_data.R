ae4b52453a56533f9d728f453ff160275d064286
jfberner <jfredericoberner@gmail.com>
2022-07-12 12:46:50 -0300

    variable correction in line 25

--- R/02_importing_data.R
+++ R/02_importing_data.R
@@ -22,7 +22,7 @@ length(data_list)
 # Inspecting data
 head(data_list$envir)
 dim(data_list$envir)
-summary(data_list$envir)
+summary(envir$envir)
 
 # Output 1: summary table ------------------------------------------------------
 
@@ -62,7 +62,7 @@ head(comm)
 
 # Sum of species abundances across sites
 comm_sum <- apply(comm[, -1], 2, sum)
-colSums(comm[, -1]) # does the same thing as line above
+colSums(comm[, -1])
 
 # Plotting a species abundance curve
 plot(sort(comm_sum, decreasing = TRUE), las = 1, bty = "l",
