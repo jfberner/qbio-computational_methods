98a1fd7e094ea62a0c262448096f84ef57f13fab
jfberner <jfredericoberner@gmail.com>
2022-07-21 12:27:10 -0300

    Plotting Covid time-series, working with Date class

---
+++ R/10_Time-Series.R
@@ -0,0 +1,43 @@
+# --------------------------------------------------#
+# Scientific computing
+# ICTP/Serrapilheira 2022
+# Sara's Last class - Time Series
+# First version 2022-07-22
+# jfb
+# --------------------------------------------------#
+
+##### Libs #####
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(zoo)
+##### Time Series in R #####
+covid <- read.csv("data/raw/covid19-dd7bc8e57412439098d9b25129ae6f35.csv")
+
+head(covid)
+
+# First checking the class
+class(covid$date)
+
+# Changing to date format
+covid$date <- as_date(covid$date)
+# Checking the class
+class(covid$date)
+
+# Now we can make numeric operations
+range(covid$date)
+
+# Plotting a time-series with ggplot2
+ggplot(covid) +
+  geom_line(aes(x = date, y = new_confirmed)) +
+  theme_minimal()
+
+# Oops. We have negative cases and will substitute the negative values per zero.
+covid$new_confirmed[covid$new_confirmed < 0] <- 0
+# and plot again
+
+ggplot(covid) +
+  geom_line(aes(x = date, y = new_confirmed)) +
+  theme_minimal() +
+  labs(x = "Date", y = "New cases")
+
