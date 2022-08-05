2887e9805ccbe81e1dace7b4f4bca0b809bc13d4
jfberner <jfredericoberner@gmail.com>
2022-08-05 13:08:50 -0300

    Added pca_env at the end

--- R/16_Multivariate-Analysis.R
+++ R/16_Multivariate-Analysis.R
@@ -68,10 +68,16 @@ plot(pca)
 
 # playing around
 pca_env <- rda(dune.env) # doesn't work, we have to work on the categorical variables OR remove them. We'll remove them:
-dune.env1 <- dune.env[,c("A1","Moisture","Manure")]
-dune.env1$A1 <- as.numeric(dune.env1$A1)
+dune.env1 <- dune.env[,c("A1","Moisture","Manure")] # create a new dune.env only containing the continuous variables
+dune.env1$A1 <- as.numeric(dune.env1$A1) # change the factors to numeric
 dune.env1$Moisture <- as.numeric(dune.env1$Moisture)
 dune.env1$Manure <- as.numeric(dune.env1$Manure)
-pca_env <- rda(dune.env1)
 
-plot(pca_env)
+
+pca_env <- rda(dune.env1) # PC the shit out of it
+pca_env # for a resume of PC contribution
+# summary(pca_env) # for more info
+plot(pca_env) # check it out
+# thickness of soil (A1) and Moisture look very correlated but check
+# cor(dune.env1) # and see its just 0.47, so in the other dimensions moisture and A1 should not look so correlated
+# plot(pca_env, choices = c(2,3)) # and they point in opposite directions, as expected :)
